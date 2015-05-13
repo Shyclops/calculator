850d67b5e547b46e63a0c088b387c19410cc17e3
Tony Xiao <tonyxiao22@gmail.com>
2015-05-13 11:02:09 -0400

    setup tokenizing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate calculator;
 
 fn main() {
-	println!("{}", calculator::reverse_polish_notation::parser::parse(format!("3-5*6+7/4")));
+	println!("{}", calculator::reverse_polish_notation::parser::parse(format!("32-50*06+74/24nil")));
 }
